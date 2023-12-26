19156ceb7da02d8ab0ab35570a2f6b6347a92927
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-26 17:50:12 -0300

    Add jobs published in current month stat

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StatisticsService } from './statistics.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'vgm-statistics',
@@ -8,6 +11,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+  jobsPublishedInCurrentMonthCount!: Observable<number>
 
+  constructor(private statisticsService: StatisticsService){}
+
+  ngOnInit(): void {
+    this.jobsPublishedInCurrentMonthCount = this.statisticsService.getJobsPublishedInCurrentMonthCount()
+  }
 }
--- src/app/statistics/statistics.service.ts
+++ src/app/statistics/statistics.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { JobService } from '../job/job.service';
-import { Observable, first, map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +20,26 @@ export class StatisticsService {
 
       return citiesMap
     }))
+  }
+
+  getJobsPublishedInCurrentMonthCount(): Observable<number> {
+    const today = new Date()
+    
+    return this.jobService.jobs$.pipe(map((jobs) => {
+      return jobs.filter((job) => {
+        const jobPublishedDate = new Date(job.publishedDate)
+        return this.haveSameMonth(jobPublishedDate, today)
+      })
+    }), map((jobs) => jobs.length))
+  }
+
+  private haveSameMonth(date1: Date, date2: Date) {
+    const month1 = date1.getMonth();
+    const year1 = date1.getFullYear();
+    
+    const month2 = date2.getMonth();
+    const year2 = date2.getFullYear();
+
+    return month1 === month2 && year1 === year2;
   }
 }

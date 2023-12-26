69fc37152464d32301f371abc4c6076816c56fdf
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-26 16:09:38 -0300

    Add job service

---
+++ src/app/job/job.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { DynamoService } from '../dynamo/dynamo.service';
+import { Job } from './job.model';
+import { BehaviorSubject, Observable, first, map, take } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+  private _jobs$ = new BehaviorSubject<Job[]>([])
+  jobs$ = this._jobs$.asObservable()
+
+  constructor(private dynamoService: DynamoService) {
+    this.dynamoService.scanJobs().pipe(first(), map((output) => output.Items as Job[])).subscribe((jobs) => this._jobs$.next(jobs))
+  }
+}

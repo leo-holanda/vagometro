302f5fd6a65c2e5e6f43352d49697141ba0fb7a0
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-12-29 17:37:12 -0300

    Fix state abbreviation pipe

--- src/app/shared/pipes/state-abbreviation.pipe.ts
+++ src/app/shared/pipes/state-abbreviation.pipe.ts
@@ -1,14 +1,45 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+type StatesDict = {
+  [id: string]: string;
+};
+
 @Pipe({
   name: 'stateAbbreviation',
   standalone: true,
 })
 export class StateAbbreviationPipe implements PipeTransform {
+  states: StatesDict = {
+    Acre: 'AC',
+    Alagoas: 'AL',
+    Amapá: 'AP',
+    Amazonas: 'AM',
+    Bahia: 'BA',
+    Ceará: 'CE',
+    'Espírito Santo': 'ES',
+    Goiás: 'GO',
+    Maranhão: 'MA',
+    'Mato Grosso': 'MT',
+    'Mato Grosso do Sul': 'MS',
+    'Minas Gerais': 'MG',
+    Pará: 'PA',
+    Paraíba: 'PB',
+    Paraná: 'PR',
+    Pernambuco: 'PE',
+    Piauí: 'PI',
+    'Rio de Janeiro': 'RJ',
+    'Rio Grande do Norte': 'RN',
+    'Rio Grande do Sul': 'RS',
+    Rondônia: 'RO',
+    Roraima: 'RR',
+    'Santa Catarina': 'SC',
+    'São Paulo': 'SP',
+    Sergipe: 'SE',
+    Tocantins: 'TO',
+    'Distrito Federal': 'DF',
+  };
+
   transform(value: string, ...args: unknown[]): unknown {
-    const splittedState = value.split(' ');
-    return `${splittedState[0][0]}${
-      splittedState[splittedState.length - 1][0]
-    }`;
+    return this.states[value];
   }
 }

843a75aa3e5cfc60a8967f3e8384b27802a5eff4
Leonardo <leo.uldholanda@gmail.com>
2024-01-02 15:05:38 -0300

    Add location column in job list

--- src/app/job/job-list/job-list.component.ts
+++ src/app/job/job-list/job-list.component.ts
@@ -4,11 +4,12 @@ import { JobService } from '../job.service';
 import { Observable } from 'rxjs';
 import { Job } from '../job.model';
 import { translations } from 'src/app/statistics/ranks/type-rank/type-rank.translations';
+import { StateAbbreviationPipe } from 'src/app/shared/pipes/state-abbreviation.pipe';
 
 @Component({
   selector: 'vgm-job-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StateAbbreviationPipe],
   templateUrl: './job-list.component.html',
   styleUrls: ['./job-list.component.scss'],
 })

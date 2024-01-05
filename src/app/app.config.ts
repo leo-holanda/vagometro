0ac2463fb8c58c8af46e526ae6b26b42d2771d18
Leonardo <leo.uldholanda@gmail.com>
2024-01-05 09:19:22 -0300

    Fix scroll position when chaning routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 
+//References: https://stackoverflow.com/questions/76318742/configuring-scroll-restoration-for-angular-standalone-router
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes, inMemoryScrollingFeature)],
 };

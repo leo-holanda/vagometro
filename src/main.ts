6ba5ec9138a771bbd5808cbca9679ce9ddeb8d00
Leonardo <leo.uldholanda@gmail.com>
2024-01-08 10:08:09 -0300

    Add vercel web analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)

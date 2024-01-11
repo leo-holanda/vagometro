8044853d4873baad71134edeb15be5b6c54edc64
Leonardo <leo.uldholanda@gmail.com>
2024-01-11 14:30:01 -0300

    feat: add education-rank component

---
+++ src/app/statistics/ranks/education-rank/education-rank.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EducationRankComponent } from './education-rank.component';
+
+describe('EducationRankComponent', () => {
+  let component: EducationRankComponent;
+  let fixture: ComponentFixture<EducationRankComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [EducationRankComponent]
+    });
+    fixture = TestBed.createComponent(EducationRankComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/statistics/ranks/education-rank/education-rank.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'vgm-education-rank',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './education-rank.component.html',
+  styleUrls: ['./education-rank.component.scss'],
+})
+export class EducationRankComponent {}

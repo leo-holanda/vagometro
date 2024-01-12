df358496818e0dc4f48a0c6a408b4acdabf4fe2a
Leonardo <leo.uldholanda@gmail.com>
2024-01-12 13:09:45 -0300

    fix: education overview route name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,7 +43,7 @@ export const routes: Routes = [
     component: DisabilityStatusesOverviewComponent,
   },
   {
-    path: 'education',
+    path: 'educacao',
     component: EducationOverviewComponent,
   },
   {

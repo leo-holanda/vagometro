34a1b202e54e2c67131f38933381c0b65a52a13e
Leonardo <leo.uldholanda@gmail.com>
2024-01-29 15:57:47 -0300

    feat: add comparative data in job count component

--- frontend/src/app/statistics/job-count/job-count.component.ts
+++ frontend/src/app/statistics/job-count/job-count.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { StatisticsService } from '../statistics.service';
 import { JobService } from 'src/app/job/job.service';
 import { TimeWindows } from 'src/app/job/job.types';
 import { RouterModule } from '@angular/router';
+import { MonthlyComparativeData } from '../ranks/months-rank/months-rank.types';
 
 @Component({
   selector: 'vgm-job-count',
@@ -19,11 +20,20 @@ export class JobCountComponent {
   currentTimeWindow$!: Observable<TimeWindows>;
   timeWindows = TimeWindows;
 
+  lastMonthDifference$: Observable<MonthlyComparativeData>;
+  lastYearDifference$: Observable<MonthlyComparativeData>;
+
   constructor(
     private statisticsService: StatisticsService,
     private jobService: JobService
   ) {
     this.currentTimeWindow$ = this.jobService.currentTimeWindow$;
     this.jobsCount$ = this.statisticsService.getJobsCount();
+    this.lastMonthDifference$ = this.statisticsService
+      .getMonthlyComparison()
+      .pipe(map((monthlyData) => monthlyData[0]));
+    this.lastYearDifference$ = this.statisticsService
+      .getAnnualComparison()
+      .pipe(map((annualData) => annualData[0]));
   }
 }

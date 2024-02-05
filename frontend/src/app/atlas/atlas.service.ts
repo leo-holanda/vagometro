8ce06e50bb66b0627ca016b1b62aa262ea5d7b80
Leonardo <leo.uldholanda@gmail.com>
2024-02-05 14:41:38 -0300

    fix: multiple connections being open with database

--- frontend/src/app/atlas/atlas.service.ts
+++ frontend/src/app/atlas/atlas.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import * as Realm from 'realm-web';
-import { Observable, switchMap, defer } from 'rxjs';
+import {
+  Observable,
+  defer,
+  shareReplay,
+  mergeMap,
+  first,
+  switchMap,
+} from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { GupyJob } from '../job-sources/gupy/gupy.types';
 
@@ -8,83 +15,78 @@ import { GupyJob } from '../job-sources/gupy/gupy.types';
   providedIn: 'root',
 })
 export class AtlasService {
-  private app = new Realm.App({ id: environment.ATLAS_APP_ID });
-  private mongoDB: any;
+  //TODO: Find where the MongoDB types are and set it here
   private mobileJobsCollection: any;
   private devopsJobsCollection: any;
   private uiuxJobsCollection: any;
   private webdevJobsCollection: any;
   private dataJobsCollection: any;
 
-  constructor() {}
+  private connectionObservable$: any;
+
+  constructor() {
+    this.connectionObservable$ = defer(() => this.openConnection()).pipe(
+      shareReplay()
+    );
+  }
 
   private async openConnection(): Promise<void> {
+    const app = new Realm.App({ id: environment.ATLAS_APP_ID });
     const credentials = Realm.Credentials.anonymous();
-    await this.app.logIn(credentials);
-    if (this.app.currentUser) {
-      this.mongoDB = this.app.currentUser.mongoClient(
-        environment.DATA_SOURCE_NAME
-      );
+    await app.logIn(credentials);
 
-      this.mobileJobsCollection = this.mongoDB
+    if (app.currentUser) {
+      const mongoDB = app.currentUser.mongoClient(environment.DATA_SOURCE_NAME);
+
+      this.mobileJobsCollection = mongoDB
         .db(environment.DATABASE_NAME)
         .collection(environment.MOBILE_COLLECTION_NAME);
 
-      this.devopsJobsCollection = this.mongoDB
+      this.devopsJobsCollection = mongoDB
         .db(environment.DATABASE_NAME)
         .collection(environment.DEVOPS_COLLECTION_NAME);
 
-      this.uiuxJobsCollection = this.mongoDB
+      this.uiuxJobsCollection = mongoDB
         .db(environment.DATABASE_NAME)
         .collection(environment.UIUX_COLLECTION_NAME);
 
-      this.webdevJobsCollection = this.mongoDB
+      this.webdevJobsCollection = mongoDB
         .db(environment.DATABASE_NAME)
         .collection(environment.WEBDEV_COLLECTION_NAME);
 
-      this.dataJobsCollection = this.mongoDB
+      this.dataJobsCollection = mongoDB
         .db(environment.DATABASE_NAME)
         .collection(environment.DATA_COLLECTION_NAME);
     }
   }
 
   getDataJobs(): Observable<GupyJob[]> {
-    return defer(() => this.openConnection()).pipe(
-      switchMap(() =>
-        defer(() => this.dataJobsCollection.find() as Observable<GupyJob[]>)
-      )
+    return this.connectionObservable$.pipe(
+      switchMap(() => this.dataJobsCollection.find() as Observable<GupyJob[]>)
     );
   }
 
   getWebDevJobs(): Observable<GupyJob[]> {
-    return defer(() => this.openConnection()).pipe(
-      switchMap(() =>
-        defer(() => this.webdevJobsCollection.find() as Observable<GupyJob[]>)
-      )
+    return this.connectionObservable$.pipe(
+      switchMap(() => this.webdevJobsCollection.find() as Observable<GupyJob[]>)
     );
   }
 
   getUIUXJobs(): Observable<GupyJob[]> {
-    return defer(() => this.openConnection()).pipe(
-      switchMap(() =>
-        defer(() => this.uiuxJobsCollection.find() as Observable<GupyJob[]>)
-      )
+    return this.connectionObservable$.pipe(
+      switchMap(() => this.uiuxJobsCollection.find() as Observable<GupyJob[]>)
     );
   }
 
   getMobileJobs(): Observable<GupyJob[]> {
-    return defer(() => this.openConnection()).pipe(
-      switchMap(() =>
-        defer(() => this.mobileJobsCollection.find() as Observable<GupyJob[]>)
-      )
+    return this.connectionObservable$.pipe(
+      switchMap(() => this.mobileJobsCollection.find() as Observable<GupyJob[]>)
     );
   }
 
   getDevOpsJobs(): Observable<GupyJob[]> {
-    return defer(() => this.openConnection()).pipe(
-      switchMap(() =>
-        defer(() => this.devopsJobsCollection.find() as Observable<GupyJob[]>)
-      )
+    return this.connectionObservable$.pipe(
+      switchMap(() => this.devopsJobsCollection.find() as Observable<GupyJob[]>)
     );
   }
 }

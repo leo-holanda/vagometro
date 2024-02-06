35bc1e958f5c8c630fc60b2c6b7ded462d94e19c
Leonardo <leo.uldholanda@gmail.com>
2024-02-06 15:05:28 -0300

    fix: improve job source selector

--- frontend/src/app/job-sources/job-source-selector/job-source-selector.component.ts
+++ frontend/src/app/job-sources/job-source-selector/job-source-selector.component.ts
@@ -16,18 +16,31 @@ import {
   styleUrls: ['./job-source-selector.component.scss'],
 })
 export class JobSourceSelectorComponent {
-  gupyJobSources!: JobCollectionsMap;
-  githubJobSources!: JobCollectionsMap;
+  private gupyJobCollections!: JobCollectionsMap;
+  private githubJobCollections!: JobCollectionsMap;
+  selectedJobCollections!: JobCollectionsMap;
 
   jobCollectionsMap = jobCollectionsMap;
+  selectedJobSource = JobSources.gupy;
+  jobSources = JobSources;
 
   constructor(private jobSourcesService: JobSourcesService) {
-    this.gupyJobSources = this.getCollectionByJobSource(JobSources.gupy);
-    this.githubJobSources = this.getCollectionByJobSource(JobSources.github);
+    this.gupyJobCollections = this.getCollectionByJobSource(JobSources.gupy);
+    this.githubJobCollections = this.getCollectionByJobSource(
+      JobSources.github
+    );
+    this.selectedJobCollections = this.gupyJobCollections;
   }
 
   toggleJobCollection(jobCollection: string): void {
     this.jobSourcesService.toggleJobCollection(jobCollection as JobCollections);
+  }
+
+  setJobSource(jobSource: JobSources): void {
+    this.selectedJobSource = jobSource;
+    if (jobSource == JobSources.github)
+      this.selectedJobCollections = this.githubJobCollections;
+    else this.selectedJobCollections = this.gupyJobCollections;
   }
 
   private getCollectionByJobSource(jobSource: JobSources): JobCollectionsMap {

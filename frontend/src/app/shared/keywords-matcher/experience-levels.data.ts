4996741874e09dbc11b753d5376281d673bec398
Leonardo <leo.uldholanda@gmail.com>
2024-02-16 08:33:05 -0300

    feat: add new keywords in experience levels pool

--- frontend/src/app/shared/keywords-matcher/experience-levels.data.ts
+++ frontend/src/app/shared/keywords-matcher/experience-levels.data.ts
@@ -75,6 +75,8 @@ export const experienceLevelRelatedTerms: ExperienceLevelRelatedTerms = {
 
   Further observation is necessary to validate this
 */
+
+// Create prefix (experiencia) and suffix (comprovada, em, com, etc) logic to automate this object creation
 export const multiWordExperienceLevelRelatedTerms: ExperienceLevelRelatedTerms = {
   'experiencia necessaria': ExperienceLevels.unknownWithExperience,
   'experiencia na funcao': ExperienceLevels.unknownWithExperience,
@@ -86,76 +88,89 @@ export const multiWordExperienceLevelRelatedTerms: ExperienceLevelRelatedTerms =
   'experiencia com': ExperienceLevels.unknownWithExperience,
   'experiencia nas': ExperienceLevels.unknownWithExperience,
   'experiencia no': ExperienceLevels.unknownWithExperience,
+  'experiencia de': ExperienceLevels.unknownWithExperience,
 
   'vivencia comprovada': ExperienceLevels.unknownWithExperience,
   'vivencia em': ExperienceLevels.unknownWithExperience,
   'vivencia com': ExperienceLevels.unknownWithExperience,
   'vivencia nas': ExperienceLevels.unknownWithExperience,
   'vivencia no': ExperienceLevels.unknownWithExperience,
+  'vivencia de': ExperienceLevels.unknownWithExperience,
 
   'experiencia previa comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia previa em': ExperienceLevels.unknownWithExperience,
   'experiencia previa com': ExperienceLevels.unknownWithExperience,
   'experiencia previa nas': ExperienceLevels.unknownWithExperience,
   'experiencia previa no': ExperienceLevels.unknownWithExperience,
+  'experiencia previa de': ExperienceLevels.unknownWithExperience,
 
   'experiencia significativa comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia significativa em': ExperienceLevels.unknownWithExperience,
   'experiencia significativa com': ExperienceLevels.unknownWithExperience,
   'experiencia significativa nas': ExperienceLevels.unknownWithExperience,
   'experiencia significativa no': ExperienceLevels.unknownWithExperience,
+  'experiencia significativa de': ExperienceLevels.unknownWithExperience,
 
   'experiencia desejavel comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia desejavel em': ExperienceLevels.unknownWithExperience,
   'experiencia desejavel com': ExperienceLevels.unknownWithExperience,
   'experiencia desejavel nas': ExperienceLevels.unknownWithExperience,
   'experiencia desejavel no': ExperienceLevels.unknownWithExperience,
+  'experiencia desejavel de': ExperienceLevels.unknownWithExperience,
 
   'experiencia pratica comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia pratica em': ExperienceLevels.unknownWithExperience,
   'experiencia pratica com': ExperienceLevels.unknownWithExperience,
   'experiencia pratica nas': ExperienceLevels.unknownWithExperience,
   'experiencia pratica no': ExperienceLevels.unknownWithExperience,
+  'experiencia pratica de': ExperienceLevels.unknownWithExperience,
 
   'experiencia solida comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia solida em': ExperienceLevels.unknownWithExperience,
   'experiencia solida com': ExperienceLevels.unknownWithExperience,
   'experiencia solida nas': ExperienceLevels.unknownWithExperience,
   'experiencia solida no': ExperienceLevels.unknownWithExperience,
+  'experiencia solida de': ExperienceLevels.unknownWithExperience,
 
   'experiencia necessaria comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia necessaria em': ExperienceLevels.unknownWithExperience,
   'experiencia necessaria com': ExperienceLevels.unknownWithExperience,
   'experiencia necessaria nas': ExperienceLevels.unknownWithExperience,
   'experiencia necessaria no': ExperienceLevels.unknownWithExperience,
+  'experiencia necessaria de': ExperienceLevels.unknownWithExperience,
 
   'experiencia especifica comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia especifica em': ExperienceLevels.unknownWithExperience,
   'experiencia especifica com': ExperienceLevels.unknownWithExperience,
   'experiencia especifica nas': ExperienceLevels.unknownWithExperience,
   'experiencia especifica no': ExperienceLevels.unknownWithExperience,
+  'experiencia especifica de': ExperienceLevels.unknownWithExperience,
 
   'experiencia profissional comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia profissional em': ExperienceLevels.unknownWithExperience,
   'experiencia profissional com': ExperienceLevels.unknownWithExperience,
   'experiencia profissional nas': ExperienceLevels.unknownWithExperience,
   'experiencia profissional no': ExperienceLevels.unknownWithExperience,
+  'experiencia profissional de': ExperienceLevels.unknownWithExperience,
 
   'experiencia avancada comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia avancada em': ExperienceLevels.unknownWithExperience,
   'experiencia avancada com': ExperienceLevels.unknownWithExperience,
   'experiencia avancada nas': ExperienceLevels.unknownWithExperience,
   'experiencia avancada no': ExperienceLevels.unknownWithExperience,
+  'experiencia avancada de': ExperienceLevels.unknownWithExperience,
 
   'experiencia avancado comprovada': ExperienceLevels.unknownWithExperience, //sic
   'experiencia avancado em': ExperienceLevels.unknownWithExperience, //sic
   'experiencia avancado com': ExperienceLevels.unknownWithExperience, //sic
   'experiencia avancado nas': ExperienceLevels.unknownWithExperience, //sic
   'experiencia avancado no': ExperienceLevels.unknownWithExperience, //sic
+  'experiencia avancado de': ExperienceLevels.unknownWithExperience, //sic
 
   'experiencia robusta e diversificada comprovada': ExperienceLevels.unknownWithExperience,
   'experiencia robusta e diversificada em': ExperienceLevels.unknownWithExperience,
   'experiencia robusta e diversificada com': ExperienceLevels.unknownWithExperience,
   'experiencia robusta e diversificada nas': ExperienceLevels.unknownWithExperience,
   'experiencia robusta e diversificada no': ExperienceLevels.unknownWithExperience,
+  'experiencia robusta e diversificada de': ExperienceLevels.unknownWithExperience,
 };

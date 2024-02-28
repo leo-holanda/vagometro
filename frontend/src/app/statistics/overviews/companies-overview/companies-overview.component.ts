d58cde8036b63ec5ae70180375b1246886401740
Leonardo <leo.uldholanda@gmail.com>
2024-02-28 14:28:07 -0300

    feat: add generic rank on companies overview

--- frontend/src/app/statistics/overviews/companies-overview/companies-overview.component.ts
+++ frontend/src/app/statistics/overviews/companies-overview/companies-overview.component.ts
@@ -1,46 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CompanyData } from '../../ranks/companies-rank/companies-rank.model';
 import { Observable } from 'rxjs';
 import { Job } from 'src/app/job/job.types';
 import { JobService } from 'src/app/job/job.service';
 import { StatisticsService } from '../../statistics.service';
-import { WorkplaceRankComponent } from '../../ranks/workplace-rank/workplace-rank.component';
-import { KeywordsRankComponent } from '../../ranks/keywords-rank/keywords-rank.component';
-import { TypeRankComponent } from '../../ranks/type-rank/type-rank.component';
 import { PublicationChartComponent } from '../../charts/publication-chart/publication-chart.component';
 import { JobListComponent } from 'src/app/job/job-list/job-list.component';
-import { ExperienceLevelsRankComponent } from '../../ranks/experience-levels-rank/experience-levels-rank.component';
 import { trackByCompany } from 'src/app/shared/track-by-functions';
-import { EducationRankComponent } from '../../ranks/education-rank/education-rank.component';
-import { LanguagesRankComponent } from '../../ranks/languages-rank/languages-rank.component';
 import { JobPostingsComparisonComponent } from '../../comparisons/job-postings-comparison/job-postings-comparison.component';
+import { RankComponent } from '../../ranks/rank/rank.component';
+import { RankData, RankTypes } from '../../ranks/rank/rank.types';
 
 @Component({
   selector: 'vgm-companies-overview',
   standalone: true,
   imports: [
     CommonModule,
-    WorkplaceRankComponent,
-    KeywordsRankComponent,
-    TypeRankComponent,
     PublicationChartComponent,
     JobListComponent,
-    ExperienceLevelsRankComponent,
-    EducationRankComponent,
-    LanguagesRankComponent,
     JobPostingsComparisonComponent,
+    RankComponent,
   ],
   templateUrl: './companies-overview.component.html',
   styleUrls: ['./companies-overview.component.scss'],
 })
 export class CompaniesOverviewComponent implements OnInit {
-  companiesRank$!: Observable<CompanyData[]>;
+  companiesRank$!: Observable<RankData[]>;
   jobsQuantity!: number;
   selectedCompany!: string;
   jobsByCompany$!: Observable<Job[]>;
 
   trackByCompany = trackByCompany;
+  rankTypes = RankTypes;
 
   constructor(
     private statisticsService: StatisticsService,

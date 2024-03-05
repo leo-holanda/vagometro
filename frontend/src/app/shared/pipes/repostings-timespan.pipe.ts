c832c101a384eb8b7e3a9b8cc28750b6f3ec74b3
Leonardo <leo.uldholanda@gmail.com>
2024-03-05 16:00:56 -0300

    fix: repostings data on job list card

--- frontend/src/app/shared/pipes/repostings-timespan.pipe.ts
+++ frontend/src/app/shared/pipes/repostings-timespan.pipe.ts
@@ -6,13 +6,26 @@ import { Job } from 'src/app/job/job.types';
   standalone: true,
 })
 export class RepostingsTimespanPipe implements PipeTransform {
-  transform(value: Job[], ...args: unknown[]): unknown {
-    const startDate = value[0].publishedDate;
-    const endDate = value[value.length - 1].publishedDate;
-    const differenceInDays = (
-      (endDate.getTime() - startDate.getTime()) /
-      (1000 * 3600 * 24)
-    ).toFixed(0);
+  transform(job: Job): number {
+    const jobsToConsider = [job, ...job.repostings];
+    const startDate = this.findEarliestDate(jobsToConsider);
+    const endDate = this.findLatestDate(jobsToConsider);
+    const differenceInDays = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
     return differenceInDays;
+  }
+
+  private findEarliestDate(jobs: Job[]): Date {
+    return jobs.reduce((acc, job) => {
+      if (job.publishedDate < acc) return job.publishedDate;
+      return acc;
+    }, new Date());
+  }
+
+  private findLatestDate(jobs: Job[]): Date {
+    const latestDate = jobs[0].publishedDate;
+    return jobs.reduce((acc, job) => {
+      if (job.publishedDate > acc) return job.publishedDate;
+      return acc;
+    }, latestDate);
   }
 }

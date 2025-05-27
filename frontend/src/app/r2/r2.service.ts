f6308935ace7dee02f30e38696dccb3b013c5a0f
Leonardo Holanda <leo.uldholanda@gmail.com>
2025-05-27 11:53:32 +0000

    Add temporary fix to NaN string error when parsing json job files

--- frontend/src/app/r2/r2.service.ts
+++ frontend/src/app/r2/r2.service.ts
@@ -8,35 +8,35 @@ import { GupyJob } from '../job-sources/gupy/gupy.types';
 import { LinkedInJob } from '../job-sources/linkedin/linked-in.types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class R2Service {
-
-  constructor() { }
+  constructor() {}
 
   async getJobs(
-      //TODO: Auto generate this types
-      collectionName: string,
-      year: number,
-      quarter: Quarters,
-    ): Promise<GitHubJob[] | GupyJob[] | LinkedInJob[]> {
-      // https://gildas-lormeau.github.io/zip.js/
-      // Try catch is not necessary. Errors are handled in Job Source Service.
+    //TODO: Auto generate this types
+    collectionName: string,
+    year: number,
+    quarter: Quarters,
+  ): Promise<GitHubJob[] | GupyJob[] | LinkedInJob[]> {
+    // https://gildas-lormeau.github.io/zip.js/
+    // Try catch is not necessary. Errors are handled in Job Source Service.
 
-      const key = this.buildKey(collectionName, year, quarter);
-      const zippedJobs = await fetch(`${environment.GITHUB_WORKER_URL}/${key}`);
-      const zipFileReader = new zip.BlobReader(await zippedJobs.blob());
-  
-      const zippedJobsWriter = new zip.TextWriter();
-      const zipReader = new zip.ZipReader(zipFileReader);
-      const firstEntry = (await zipReader.getEntries()).shift();
-      const jobs = await firstEntry!.getData!(zippedJobsWriter);
-      await zipReader.close();
+    const key = this.buildKey(collectionName, year, quarter);
+    const zippedJobs = await fetch(`${environment.GITHUB_WORKER_URL}/${key}.zip`);
+    const zipFileReader = new zip.BlobReader(await zippedJobs.blob());
 
-      return JSON.parse(jobs);
-    }
+    const zippedJobsWriter = new zip.TextWriter();
+    const zipReader = new zip.ZipReader(zipFileReader);
+    const firstEntry = (await zipReader.getEntries()).shift();
+    const jobs = await firstEntry!.getData!(zippedJobsWriter);
+    await zipReader.close();
 
-    private buildKey(collectionName: string, year: number, quarter: Quarters): string {
-      return `${collectionName}_${year}_${quarter}`
-    }
+    const processedJobs = jobs.replace(/\bNaN\b/g, '"NaN"');
+    return JSON.parse(processedJobs);
+  }
+
+  private buildKey(collectionName: string, year: number, quarter: Quarters): string {
+    return `${collectionName}_${year}_${quarter}`;
+  }
 }
